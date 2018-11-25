e4857e8b840d97264d8d57567372357272a3ade8
J. Gabriel Maia <jonagabriel@gmail.com>
2018-11-25 16:57:50 -0200

    Menu de rodape atualizado

--- app/src/main/java/com/example/gabrielmaia/chocolapp/MainActivity.java
+++ app/src/main/java/com/example/gabrielmaia/chocolapp/MainActivity.java
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_candies);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_history);
                     return true;
             }
             return false;
